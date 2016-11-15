091407949e08e644d971ee27344d7cb76fcf86a1
guoxiaozhong <gxz04220427@163.com>
2016-11-15 15:28:14 +0800

    建库

--- src/test/java/org/bpl/gear/te/test/GtMeasureResultTest.java
+++ src/test/java/org/bpl/gear/te/test/GtMeasureResultTest.java
@@ -17,5 +17,6 @@ public class GtMeasureResultTest {
         FileOperate fileOperate = new FileOperate();
         List<Double> datas = fileOperate.readFileLines("/Users/guoxiaozhong/项目/汽车齿轮高效配对/E04.参考资料/实验数据/试验台实验结果/特型齿轮/实验数据/001/TE_Test_001_left.txt");
         fileOperate.writeArraytoFile("/Users/guoxiaozhong/项目/汽车齿轮高效配对/E04.参考资料/实验数据/试验台实验结果/特型齿轮/实验数据/001/", "11111.txt", datas);
+
     }
 }
